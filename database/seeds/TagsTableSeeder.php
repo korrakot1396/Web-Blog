2b1a32a5e332ea207fcfaa5d586191af36fc5c8c
korrakot1396 <korrakot.tr@ku.th>
2020-06-26 20:56:09 +0700

    update

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -16,15 +16,15 @@ class TagsTableSeeder extends Seeder
         $tags = [
             [
                 'tag' => 'Laravel',
-                'title' => 'Laravel เป็น Php framework',
-                'meta_description'  => 'Laravel เป็น Php framework',
+                'title' => 'Laravel เป็น PHP Framework รูปแบบ MVC และเป็นที่นิยมใช้มากของนักพัฒนาเระบบ',
+                'meta_description'  => 'หรือเว็บแอพพลิเคชั่นในปัจจุบันเพราะมีความสามรถที่ช่วยในการทำงานให้ง่ายและเป็นระเบียบมากขึ้นซึ่งพัฒนาโดยทีม Taylor Otwell',
                 'created_at'  => now(),
                 'updated_at'  => now()
             ],
             [
                 'tag' => 'Html',
-                'title' => 'Laravel เป็น Php framework',
-                'meta_description'  => 'Laravel เป็น Php framework',
+                'title' => 'HTML เป็นภาษาประเภท Markup Language ที่ใช้ในการสร้างเว็บเพจ ',
+                'meta_description'  => 'ย่อมาจาก Hyper Text Markup Language',
                 'created_at'  => now(),
                 'updated_at'  => now()
             ],
@@ -37,18 +37,60 @@ class TagsTableSeeder extends Seeder
             ],
             [
                 'tag' => 'Css',
-                'title' => 'Laravel เป็น Php framework',
-                'meta_description'  => 'Laravel เป็น Php framework',
+                'title' => 'CSS เป็น ภาษาที่ใช้สำหรับตกแต่งเอกสาร HTML/XHTML',
+                'meta_description'  => 'ให้มีหน้าตา สีสัน ระยะห่าง พื้นหลัง เส้นขอบและอื่นๆ ตามที่ต้องการ CSS ย่อมาจาก Cascading Style Sheets',
                 'created_at'  => now(),
                 'updated_at'  => now()
             ],
             [
                 'tag' => 'Java',
-                'title' => 'Laravel เป็น Php framework',
-                'meta_description'  => 'Laravel เป็น Php framework',
+                'title' => 'Java เป็นโปรแกรมภาษาที่ถูกพัฒนามาเพื่อรองรับการออกแบบซอฟแวร์ที่มีการเชื่อมโยง Internet',
+                'meta_description'  => 'อีกทั้งยังเป็นโปรแกรมที่สนับสนุนแนวความคิดของการเขียนโปรแกรมเชิงวัตถุ หรือที่รู้จักกันดีที่เรียกว่า OOP (Object-Oriented Programming)',
                 'created_at'  => now(),
                 'updated_at'  => now()
-            ]
+            ],
+            [
+                'tag' => 'Php',
+                'title' => 'PHP เป็นภาษาสคริปต์ (Scripting Language) คำสั่งต่างๆจะเก็บในรูปของข้อความ Text',
+                'meta_description'  => 'อาจเขียนแทรกอยู่ภายในภาษา HTML หรือใช้งานอิสระก็ได้ แต่ในการใช้งานจริงมักใช้งานร่วมกับภาษา HTML ',
+                'created_at'  => now(),
+                'updated_at'  => now()
+            ],
+            [
+                'tag' => 'Golang',
+                'title' => 'Golang เป็น Open Source ที่นิยามว่า รันง่าย เชื่อถือได้ และ มีประสิทธิภาพ',
+                'meta_description'  => 'พูดง่ายๆ คือมันเร็ว กว่าภาษาอื่นๆ ทั่วๆไป และตัวมันเองก็เบามาก เวลานำมาเขียนโปรแกรม',
+                'created_at'  => now(),
+                'updated_at'  => now()
+            ],
+            [
+                'tag' => 'Python',
+                'title' => '',
+                'meta_description'  => '',
+                'created_at'  => now(),
+                'updated_at'  => now()
+            ],
+            [
+                'tag' => 'Ruby',
+                'title' => '',
+                'meta_description'  => '',
+                'created_at'  => now(),
+                'updated_at'  => now()
+            ],
+            [
+                'tag' => 'C++',
+                'title' => '',
+                'meta_description'  => '',
+                'created_at'  => now(),
+                'updated_at'  => now()
+            ],
+            [
+                'tag' => 'C#',
+                'title' => '',
+                'meta_description'  => '',
+                'created_at'  => now(),
+                'updated_at'  => now()
+            ],
         ];
 
         DB::table('tags')->insert($tags);
